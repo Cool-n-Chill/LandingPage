11dadc9ff1b863aa6b9909fa290ca7a13db06934
Cool-n-Chill <irobens34@gmail.com>
2021-01-10 23:57:58 +0300

    Portfolio block is done.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let $portfolio__item_hovered = $(
+        "<img src='img/portfolio/black-hover.png' class='portfolio__item_hovered' alt='hovered'></img>" +
+        "<img src='img/portfolio/plus.png' class='portfolio__item_plus-hovered' alt='hovered-plus'></img>"
+    );
+    $('.portfolio__item').hover(function() {
+        $(this).children('.portfolio__item-description').toggleClass('portfolio__item-description_visible');
+    });
+    $('.portfolio__item').on('mouseenter', function() {
+        $(this).append($portfolio__item_hovered);
+    });
+    $('.portfolio__item').on('mouseleave', function() {
+        $($portfolio__item_hovered).remove();
+    });
+});
