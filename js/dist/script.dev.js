9622dc05f76f6da055c3c83872484d4ebdbe6ef9
Cool-n-Chill <irobens34@gmail.com>
2021-01-13 23:49:03 +0300

    Script for team section is ready.

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -11,4 +11,32 @@ $(document).ready(function () {
   $('.portfolio__item').on('mouseleave', function () {
     $($portfolio__item_hovered).remove();
   });
+  $('.our-team__cards').children('.red-border').on('mouseenter', function () {
+    if (!$('.members-description__decription-card_red').hasClass('active-card')) {
+      $('.members-description__decription-card_red').siblings().fadeOut('fast', function () {
+        $('.members-description__decription-card_red').fadeIn('slow');
+      });
+    }
+  });
+  $('.our-team__cards').children('.green-border').on('mouseenter', function () {
+    if (!$('.members-description__decription-card_green').hasClass('active-card')) {
+      $('.members-description__decription-card_green').siblings().fadeOut('fast', function () {
+        $('.members-description__decription-card_green').fadeIn('slow');
+      });
+    }
+  });
+  $('.our-team__cards').children('.purple-border').on('mouseenter', function () {
+    if (!$('.members-description__decription-card_purple').hasClass('active-card')) {
+      $('.members-description__decription-card_purple').siblings().fadeOut('fast', function () {
+        $('.members-description__decription-card_purple').fadeIn('slow');
+      });
+    }
+  });
+  $('.our-team__cards').children('.orange-border').on('mouseenter', function () {
+    if (!$('.members-description__decription-card_orange').hasClass('active-card')) {
+      $('.members-description__decription-card_orange').siblings().fadeOut('fast', function () {
+        $('.members-description__decription-card_orange').fadeIn('slow');
+      });
+    }
+  });
 });
