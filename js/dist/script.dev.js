9651ffb82fed7e9daaae5204d8ac6d3a928b4ea2
Cool-n-Chill <irobens34@gmail.com>
2021-01-29 18:07:16 +0300

    prices

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -13,29 +13,29 @@ $(document).ready(function () {
   });
   $('.our-team__cards').children('.red-border').on('mouseenter', function () {
     if (!$('.members-description__decription-card_red').hasClass('active-card')) {
-      $('.members-description__decription-card_red').siblings().fadeOut('fast', function () {
-        $('.members-description__decription-card_red').fadeIn('slow');
+      $('.members-description__decription-card_red').siblings().fadeOut(0, function () {
+        $('.members-description__decription-card_red').fadeIn('fast');
       });
     }
   });
   $('.our-team__cards').children('.green-border').on('mouseenter', function () {
     if (!$('.members-description__decription-card_green').hasClass('active-card')) {
-      $('.members-description__decription-card_green').siblings().fadeOut('fast', function () {
-        $('.members-description__decription-card_green').fadeIn('slow');
+      $('.members-description__decription-card_green').siblings().fadeOut(0, function () {
+        $('.members-description__decription-card_green').fadeIn('fast');
       });
     }
   });
   $('.our-team__cards').children('.purple-border').on('mouseenter', function () {
     if (!$('.members-description__decription-card_purple').hasClass('active-card')) {
-      $('.members-description__decription-card_purple').siblings().fadeOut('fast', function () {
-        $('.members-description__decription-card_purple').fadeIn('slow');
+      $('.members-description__decription-card_purple').siblings().fadeOut(0, function () {
+        $('.members-description__decription-card_purple').fadeIn('fast');
       });
     }
   });
   $('.our-team__cards').children('.orange-border').on('mouseenter', function () {
     if (!$('.members-description__decription-card_orange').hasClass('active-card')) {
-      $('.members-description__decription-card_orange').siblings().fadeOut('fast', function () {
-        $('.members-description__decription-card_orange').fadeIn('slow');
+      $('.members-description__decription-card_orange').siblings().fadeOut(0, function () {
+        $('.members-description__decription-card_orange').fadeIn('fast');
       });
     }
   });
--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ $(document).ready(function(){
     });
     $('.our-team__cards').children('.red-border').on('mouseenter', ()=>{
         if (!($('.members-description__decription-card_red').hasClass('active-card'))) {
-            $('.members-description__decription-card_red').siblings().fadeOut('fast', ()=> {
-            $('.members-description__decription-card_red').fadeIn('slow');})
+            $('.members-description__decription-card_red').siblings().fadeOut(0, ()=> {
+            $('.members-description__decription-card_red').fadeIn('fast');})
         }
     });
     $('.our-team__cards').children('.green-border').on('mouseenter', ()=>{
         if (!($('.members-description__decription-card_green').hasClass('active-card'))) {
-            $('.members-description__decription-card_green').siblings().fadeOut('fast', ()=> {
-            $('.members-description__decription-card_green').fadeIn('slow');})
+            $('.members-description__decription-card_green').siblings().fadeOut(0, ()=> {
+            $('.members-description__decription-card_green').fadeIn('fast');})
             }
     });
     $('.our-team__cards').children('.purple-border').on('mouseenter', ()=>{
         if (!($('.members-description__decription-card_purple').hasClass('active-card'))) {
-            $('.members-description__decription-card_purple').siblings().fadeOut('fast', ()=> {
-            $('.members-description__decription-card_purple').fadeIn('slow');})
+            $('.members-description__decription-card_purple').siblings().fadeOut(0, ()=> {
+            $('.members-description__decription-card_purple').fadeIn('fast');})
         }
     });
     $('.our-team__cards').children('.orange-border').on('mouseenter', ()=>{
         if (!($('.members-description__decription-card_orange').hasClass('active-card'))) {
-            $('.members-description__decription-card_orange').siblings().fadeOut('fast', ()=> {
-            $('.members-description__decription-card_orange').fadeIn('slow');})
+            $('.members-description__decription-card_orange').siblings().fadeOut(0, ()=> {
+            $('.members-description__decription-card_orange').fadeIn('fast');})
         }
     });
 });
